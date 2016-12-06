68014f18c8d38385dc3ab3a7af60eaef77b22bf3
Sandy Hogan <SVern99@aol.com>
2016-12-06 17:48:17 +0000

    added custom route for contact form page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :contacts
-  
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :contacts
+  get 'contact-us', to: 'contacts#new'
   
 end
