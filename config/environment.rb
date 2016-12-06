44b5e1dd96f464a1ec994364b1202c4bce5e090d
Sandy Hogan <SVern99@aol.com>
2016-12-06 21:01:03 +0000

    adding mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'sandydevmatch.herokuapp.com',
+  :domain         => 'https://git.heroku.com/sandydevmatch.git',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
