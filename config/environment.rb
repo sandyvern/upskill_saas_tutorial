436d9ab024712f7b6be82e3da4bdfbea633dd996
Sandy Hogan <SVern99@aol.com>
2016-12-06 22:56:30 +0000

    edited domain address

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://git.heroku.com/sandydevmatch.git',
+  :domain         => 'sandydevmatch.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
